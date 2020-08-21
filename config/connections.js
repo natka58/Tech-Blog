44f70a2d6c922eabef2172a563f439e053cdc38e
Natalia Teaca <lilikoy@sbcglobal.net>
2020-08-21 11:43:08 -0700

    new connection

--- config/connections.js
+++ config/connections.js
@@ -1,11 +1,23 @@
 
-// const Sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 
-// require('dotenv').config();
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
 
 // let sequelize;
 
-// // create connection to our db
+// create connection to our db
 // const sequelize = new Sequelize(
 //   process.env.DB_NAME,
 //   process.env.DB_USER,
@@ -15,23 +27,23 @@
 //     dialect: "mysql",
 //     port: 3306,
 //   }
-// );
+//  );
 
-require('dotenv').config();
+// // require('dotenv').config();
 
-require('dotenv').config();
-const Sequelize = require('sequelize');
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  // : new Sequelize('ecommerce_db', 'root', 'Solnish1', {
-      host: 'localhost',
-      port: 3306,
-      dialect: 'mysql',
-      dialectOptions: {
-            decimalNumbers: true,
-          },
+// require('dotenv').config();
+// const Sequelize = require('sequelize');
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//   // : new Sequelize('ecommerce_db', 'root', 'Solnish1', {
+//       host: 'localhost',
+//       port: 3306,
+//       dialect: 'mysql',
+//       dialectOptions: {
+//             decimalNumbers: true,
+//           },
      
-    });
+//     });
 
 module.exports = sequelize;
