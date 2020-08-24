36f2e9291568e7572a711b9e4aca2247765d425e
Natalia Teaca <lilikoy@sbcglobal.net>
2020-08-23 20:46:16 -0700

    missing c

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
+const sequelize = require('../../config/connections');
 const withAuth = require('../../utils/auth');
 
 // Get api/users 
